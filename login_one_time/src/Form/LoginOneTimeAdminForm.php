73cdeea665828c03268ddb0dee863a86d39dbfaf
Steven <stevenl@ciandt.com>
2016-03-24 15:18:05 +0800

    finish admin form.

--- login_one_time/src/Form/LoginOneTimeAdminForm.php
+++ login_one_time/src/Form/LoginOneTimeAdminForm.php
@@ -7,6 +7,7 @@ namespace Drupal\login_one_time\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 class LoginOneTimeAdminForm extends ConfigFormBase {
   /**
@@ -27,13 +28,142 @@ class LoginOneTimeAdminForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = \Drupal::config('login_one_time.settings');
+    $form['global'] = array(
+      '#type' => 'fieldset',
+      '#title' => t("Global settings"),
+    );
+    $form['global']['login_one_time_expiry'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Link expiry'),
+      '#default_value' => $config->get('login_one_time_expiry'),
+      '#size' => strlen(PHP_INT_MAX),
+      '#maxlength' => strlen(PHP_INT_MAX),
+      '#description' => t("How long, in seconds, before links expire.  Leave blank to default to two weeks (1,209,600 seconds)."),
+    );
+    // ---testing ----
+    $form['global']['login_one_time_user_widget'] = array(
+      '#type' => 'radios',
+      '#title' => t('User selection widget'),
+      '#default_value' => $config->get('login_one_time_user_widget'),
+      '#options' => array(
+        'autocomplete' => t('Autocomplete textfield'),
+        'select' => t('Select list'),
+      ),
+      '#description' => t("If using a <em>user selection</em> widget, this configures the form element type.  For smaller sites a select list could be easier to use."),
+    );
 
+    $form['mail'] = array(
+      '#type' => 'fieldset',
+      '#title' => t("E-mail settings"),
+    );
+    $form['mail']['login_one_time_mail_message'] = array(
+      '#markup' => t('Customize <em>login one time</em> e-mail messages sent to users at the <a href="!user_settings">User settings</a> page.', array('!user_settings' => Url::fromRoute('entity.user.admin_form')->toString())),
+    );
+
+    $form['path'] = array(
+      '#type' => 'fieldset',
+      '#title' => t("Path settings"),
+    );
+    $form['path']['listed'] = array(
+      '#type' => 'fieldset',
+      '#title' => t("Listed"),
+      '#description' => t("Which paths to make available for selection."),
+    );
+
+    $form['path']['listed']['login_one_time_path_front'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("Front page"),
+      '#description' => t("The front page of the website."),
+      '#default_value' => $config->get('login_one_time_path_front'),
+    );
+    $form['path']['listed']['login_one_time_path_user'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("User page"),
+      '#description' => t("The user's account page."),
+      '#default_value' => $config->get('login_one_time_path_user'),
+    );
+    $form['path']['listed']['login_one_time_path_user_edit'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("User edit page"),
+      '#description' => t("The user's account edit page."),
+      '#default_value' => $config->get('login_one_time_path_user_edit'),
+    );
+    $form['path']['listed']['login_one_time_path_current'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("Current page"),
+      '#description' => t("Page from which <em>login one time</em> e-mail is sent."),
+      '#default_value' => $config->get('login_one_time_path_current'),
+    );
+    $form['path']['listed']['login_one_time_path_custom'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Custom paths'),
+      '#default_value' => $config->get('login_one_time_path_custom'),
+      '#description' => t("Enter one path per line.  You may also supply a display name for the path using a key|value pair, where the key is the path and the value is the display name."),
+    );
+    $form['path']['default'] = array(
+      '#type' => 'fieldset',
+      '#title' => t("Default path"),
+      '#description' => t("This is where the user will be directed to upon using a <em>login one time</em> link, or the default choice when using the <em>path selection</em> widget."),
+    );
+    $form['path']['default']['login_one_time_path_default'] = login_one_time_select_widget(
+      $config->get('login_one_time_path_default'),
+      t("Default path")
+    );
+    $form['path']['default']['login_one_time_path_default']['#required'] = FALSE;
+
+    $form['user'] = array(
+      '#type' => 'fieldset',
+      '#title' => t("User account page settings"),
+
+    );
+    $form['user']['login_one_time_user_view'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("Show <em>login one time</em> button."),
+      '#description' => t("Permitted users will be able to e-mail the link via a button on the user's account page."),
+      '#default_value' => $config->get('login_one_time_user_view'),
+    );
+    $form['user']['login_one_time_user_select'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("Show <em>path selection</em> widget."),
+      '#description' => t("Will only show when the button is shown as well."),
+      '#default_value' => $config->get('login_one_time_user_select'),
+    );
+    $form['user']['login_one_time_user_set_mail'] = array(
+      '#type' => 'checkbox',
+      '#title' => t("Show <em>email override</em> widget."),
+      '#description' => t("To send to an email address other than the account email.  Will only show when the button is shown as well."),
+      '#default_value' => $config->get('login_one_time_user_set_mail'),
+    );
+    $form['user']['login_one_time_user_ignore_current_pass'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Ignore the <em>current password</em> field'),
+      '#description' => t('Will ignore and hide the current password field when on the user edit page.'),
+      '#default_value' => $config->get('login_one_time_user_ignore_current_pass'),
+    );
+
+    return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
+    $config = $this->config('login_one_time.settings');
+    $config
+      ->set('login_one_time_expiry', $form_state->getValue('login_one_time_expiry'))
+      ->set('login_one_time_user_widget', $form_state->getValue('login_one_time_user_widget'))
+      ->set('login_one_time_mail_message', $form_state->getValue('login_one_time_mail_message'))
+      ->set('login_one_time_path_front', $form_state->getValue('login_one_time_path_front'))
+      ->set('login_one_time_path_user', $form_state->getValue('login_one_time_path_user'))
+      ->set('login_one_time_path_user_edit', $form_state->getValue('login_one_time_path_user_edit'))
+      ->set('login_one_time_path_current', $form_state->getValue('login_one_time_path_current'))
+      ->set('login_one_time_path_custom', $form_state->getValue('login_one_time_path_custom'))
+      ->set('login_one_time_path_default', $form_state->getValue('login_one_time_path_default'))
+      ->set('login_one_time_user_view', $form_state->getValue('login_one_time_user_view'))
+      ->set('login_one_time_user_select', $form_state->getValue('login_one_time_user_select'))
+      ->set('login_one_time_user_set_mail', $form_state->getValue('login_one_time_user_set_mail'))
+      ->set('login_one_time_user_ignore_current_pass', $form_state->getValue('login_one_time_user_ignore_current_pass'))
+      ->save();
   }
 }
